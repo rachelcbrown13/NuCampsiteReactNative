7efde83dde3356b081a745d357d6be2cc9447e21
Rachel Brown <rachelcbrown13@gmail.com>
2020-08-22 11:55:16 -0500

    gesture and animation

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -61,6 +61,7 @@ function RenderCampsite(props) {
     const view = React.createRef();
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+    const recognizeComment = ({dx}) => (dx > 200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -88,6 +89,9 @@ function RenderCampsite(props) {
                     ],
                     { cancelable: false }
                 );
+            } 
+            else if (recognizeComment(gestureState)) {
+                props.onShowModal();
             }
             return true;
         }

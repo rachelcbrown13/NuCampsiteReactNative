aa35e32697f1c2338c3cc54c700ea09e0c4de982
Rachel Brown <rachelcbrown13@gmail.com>
2020-08-24 21:46:31 -0500

    Send Email Trigger added

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements';
-import * as Animatable from 'react-native-animatable'
+import { Card, Button, Icon } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 
 class Contact extends Component {
@@ -10,6 +11,14 @@ class Contact extends Component {
         title: 'Contact Us'
     }
     
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['campsites@nucamp.co'],
+            subject: 'Inquiry',
+            body: 'To whom it may concern:'
+        })
+    }
+
     render () {
         return (
             <ScrollView>
@@ -21,7 +30,18 @@ class Contact extends Component {
                         <Text>Seattle, WA 98001</Text>
                         <Text style={{marginBottom:10}}>U.S.A.</Text>
                         <Text>Phone: 1-206-555-1234</Text>
-                        <Text>Email: campsites@nucamp.co</Text>                    
+                        <Text>Email: campsites@nucamp.co</Text>
+                        <Button
+                            title="Send Email"
+                            buttonStyle={{backgroundColor: '#5637DD', margin: 40}}
+                            icon={<Icon
+                                name='envelope-o'
+                                type='font-awesome'
+                                color='#fff'
+                                iconStyle={{marginRight: 10}}
+                            />}
+                            onPress={() => this.sendMail()}
+                        />                    
                     </Card>
                 </Animatable.View>
             </ScrollView>
